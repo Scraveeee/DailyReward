6ac26e31748abcf9403df94ac41990232c5f71a2
Pascal <pascal.fichtner@web.de>
2018-02-25 15:16:47 +0100

    ~ Update

--- DailyReward/src/main/java/de/scraveeee/dailyreward/events/PlayerEvents.java
+++ DailyReward/src/main/java/de/scraveeee/dailyreward/events/PlayerEvents.java
@@ -12,13 +12,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityInteractEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
